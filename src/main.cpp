b934759bf8ab85aebc25d80e082c11a7ca3a5935
UpstateBoar976 <gabriel.delbelbelluz@georgebrown.ca>
2024-01-20 19:52:08 -0500

    added yellow

--- src/main.cpp
+++ src/main.cpp
@@ -12,6 +12,7 @@ public: // makes all following variables public
     float legs[LEGS_COUNT];
     float legWidth, legHeight;
     Color colour;
+    Color yellow;
 
     void DrawTable() //Class method can use variables in the class
     {
