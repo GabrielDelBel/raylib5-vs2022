31fff446fc72fbd3ec43372aa2680281e9ca730a
UpstateBoar976 <gabriel.delbelbelluz@georgebrown.ca>
2024-01-21 12:03:51 -0500

    Added neccesary variabless

--- src/main.cpp
+++ src/main.cpp
@@ -1,22 +1,32 @@
 #include "raylib.h"
 #include "Math.h"
 #include<iostream>
-#include "string"
+#include <string>
+
+using namespace std;
 
 
  struct Achievement
  {
 	 string title;
+	 string description;
+	 int scoreValue;
 
  };
  
  struct Game
  {
+	 string name;
+	 string manufacturer;
+	 string developer;
+
 	 Achievement* Achievement = nullptr;
  };
 
  struct Platform 
  {
+	 string name;
+	 string manufacturer;
 	 Game* Game = nullptr; // game pointer named game = null pointer for now
 
  };
