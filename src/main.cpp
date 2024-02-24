6ddfbb9f32d29e720a60dfb6d983039c0a7bca4e
UpstateBoar976 <gabriel.delbelbelluz@georgebrown.ca>
2024-02-23 21:20:05 -0500

    Added more to the functions

--- src/main.cpp
+++ src/main.cpp
@@ -6,32 +6,72 @@ using namespace std;
 
 class GameObject
 {
+public:
 
+	void NormalAttack()
+	{
 
+	}
 };
 
 class Player : public GameObject
 {
+public:
+	string name;
+	int health;
 
+	void NormalAttack()
+	{
+
+	}
+
+	virtual void SpecialAttack()
+	{
+
+	}
 };
 
 class Enemy : public GameObject
 {
+public:
+	int health;
 
+	void NormalAttack()
+	{
+
+	}
+
+	virtual void SpecialAttack()
+	{
+
+	}
 };
 
 class Wizard : public Player
 {
-
+public:
+	string static specialAttackName;
+	int specialAttackAmount;
 };
 
 class Knight : public Player
 {
-
+public:
+	string static specialAttackName;
+	int specialAttackAmount;
 };
 
 class Ork : public Enemy
 {
+public:
+	string static species;
+	int* numTaunts;
+};
 
+class Undead : public Enemy
+{
+public:
+	string static species;
+	int* numTaunts;
 };
 
