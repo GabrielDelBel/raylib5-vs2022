ad2ff260c8555147993e9804cda101a06b5a0802
UpstateBoar976 <gabriel.delbelbelluz@georgebrown.ca>
2024-02-23 20:13:36 -0500

    Added all the classes I need.

--- src/main.cpp
+++ src/main.cpp
@@ -10,3 +10,28 @@ class GameObject
 
 };
 
+class Player : public GameObject
+{
+
+};
+
+class Enemy : public GameObject
+{
+
+};
+
+class Wizard : public Player
+{
+
+};
+
+class Knight : public Player
+{
+
+};
+
+class Ork : public Enemy
+{
+
+};
+
