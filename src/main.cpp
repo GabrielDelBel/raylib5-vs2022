ce58a3324369fb7b0104cc7788b3a1ee3230a456
UpstateBoar976 <gabriel.delbelbelluz@georgebrown.ca>
2024-03-22 19:57:53 -0400

    Kinda did it

--- src/main.cpp
+++ src/main.cpp
@@ -7,13 +7,31 @@ using namespace std;
 template <class T>
 class Sqaure
 {
-	T* length;
-	T* width;
+	T length;
+	T width;
+
 };
 
+template <class T>
+T Square(T length, T width)
+{
+	return length * width;
+}
 
 int main()
 {
+	int x = 2;
+	int y = 5;
+	int intResult;
+	float a = 4.37;
+	float b = 7.02;
+	float floatResult;
+
+	intResult = Square<int>(x, y);
+	cout << intResult << endl;
+	floatResult = Square<float>(a, b);
+	cout << floatResult << endl;
+	
 	
 
 };
