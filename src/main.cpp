510fd37577580348a3c0d00c8de45f6bb2deeb33
UpstateBoar976 <gabriel.delbelbelluz@georgebrown.ca>
2024-04-03 10:06:39 -0400

    1

--- src/main.cpp
+++ src/main.cpp
@@ -12,20 +12,19 @@ class ReversableWords
 public:
 	queue<char> original;
 	stack<char> backwards;
+	
 
 };
 
 string ReverseWord(string passedWord)
 {
-	
+	ReversableWords
 
 }
 
 
 int main()
 {
-	queue<char> original;
-	stack<char> backwards;
 	string word;
 
 	cout << "Please Enter a String" << endl;
